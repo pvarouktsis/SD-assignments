79be5945aaa3eb1c3dbafbc5cc9de1da3a24a786
pvarouktsis <p.varouktsis@gmail.com>
2020-06-20 18:44:13 +0300

    Implement onClick actions for sign up and sign in buttons

--- app/src/main/java/com/example/shopsmart/ui/login/LoginFragment.java
+++ app/src/main/java/com/example/shopsmart/ui/login/LoginFragment.java
@@ -4,21 +4,53 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.shopsmart.R;
+import com.example.shopsmart.ui.register.*;
 
 public class LoginFragment extends Fragment {
+    private String TAG = "TAG_LOGIN";
 
-    private LoginViewModel loginViewModel;
+    private EditText inputUsername;
+    private EditText inputPassword;
+    private Button btnLogin;
+    private Button btnRegister;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        //loginViewModel = ViewModelProviders.of(this).get(LoginViewModel.class);
         View login_view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        // Login button action
+        btnLogin = login_view.findViewById(R.id.button_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //
+            }
+        });
+
+        btnRegister = login_view.findViewById(R.id.button_register);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.main_container, new RegisterFragment())
+                        .commit();
+            }
+        });
+
+
         return login_view;
     }
 
--- app/src/main/java/com/example/shopsmart/ui/register/RegisterFragment.java
+++ app/src/main/java/com/example/shopsmart/ui/register/RegisterFragment.java
@@ -5,23 +5,55 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.shopsmart.R;
+import com.example.shopsmart.ui.login.*;
 
 public class RegisterFragment extends Fragment {
+    private String TAG = "TAG_REGISTER";
 
-    private RegisterViewModel registerViewModel;
+    private EditText inputUsername;
+    private EditText inputEmail;
+    private EditText inputPassword;
+    private Button btnRegister;
+    private Button btnLogin;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        //registerViewModel = ViewModelProviders.of(this).get(RegisterViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View register_view = inflater.inflate(R.layout.fragment_register, container, false);
+        // EditText variables here
+
+        // Register button action
+        btnRegister = register_view.findViewById(R.id.button_register);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //
+            }
+        });
+
+        // Login button action
+        btnLogin = register_view.findViewById(R.id.button_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.main_container, new LoginFragment())
+                        .commit();
+            }
+        });
+
         return register_view;
     }
+
+
 }
