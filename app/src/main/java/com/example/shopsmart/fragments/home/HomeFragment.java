767add791b29ad222609e1da10ca15e78c78dd56
pvarouktsis <p.varouktsis@gmail.com>
2020-06-22 21:31:40 +0300

    Remove unnecessary LinearLayout, actual test of search_button

--- app/src/main/java/com/example/shopsmart/fragments/home/HomeFragment.java
+++ app/src/main/java/com/example/shopsmart/fragments/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.shopsmart.fragments.home;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,12 +12,15 @@ import androidx.fragment.app.Fragment;
 import com.example.shopsmart.R;
 
 public class HomeFragment extends Fragment {
-
-    private HomeViewModel homeViewModel;
-
+    @SuppressLint("WrongViewCast")
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View home_view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // TODO
+        // ImageButton
+
         return home_view;
     }
+
 }
