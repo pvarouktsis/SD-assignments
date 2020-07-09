d941cceaf9755576b606ea72c864eead057f065f
pvarouktsis <p.varouktsis@gmail.com>
2020-07-09 19:13:43 +0300

    Add product_frame

--- app/src/main/java/com/example/shopsmart/fragments/on_sale/OnSaleFragment.java
+++ app/src/main/java/com/example/shopsmart/fragments/on_sale/OnSaleFragment.java
@@ -4,13 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.shopsmart.R;
 
@@ -19,17 +15,11 @@ public class OnSaleFragment extends Fragment {
     private OnSaleViewModel onSaleViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        onSaleViewModel =
-                ViewModelProviders.of(this).get(OnSaleViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_on_sale, container, false);
-        final TextView textView = root.findViewById(R.id.text_on_sale);
-        onSaleViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View on_sale_fragment = inflater.inflate(R.layout.fragment_on_sale, container, false);
+
+        return on_sale_fragment;
     }
 }
